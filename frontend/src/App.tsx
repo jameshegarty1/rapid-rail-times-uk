5b5fd438c772076ebfcf638aded378dfcd50c7b3
James Hegarty <jhegartyodowd@gmail.com>
2024-11-14 21:20:00 +0000

    gitignore change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AppRoutes from 'Routes';
 
 export default function App() {
+  useEffect(() => {
+    document.title = "Rail Times UK";
+  }, []);
   return <AppRoutes />;
 }
