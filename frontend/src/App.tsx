3f0df0fe81e23dcc7fdcfd9e41b269dcd05884c6
James Hegarty <jhegartyodowd@gmail.com>
2024-11-18 07:34:55 +0000

    Removed printing env vars

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import AppRoutes from 'Routes';
 
-console.log('Environment variables:', {
-  REACT_APP_API_URL: process.env.REACT_APP_API_URL,
-});
-
 export default function App() {
   return <AppRoutes />;
 }
