6bca9c566cbf37419a8c522181352a591858d143
James Hegarty <jhegartyodowd@gmail.com>
2025-01-07 00:44:24 +0000

    build

--- frontend/src/utils/axiosInstance.ts
+++ frontend/src/utils/axiosInstance.ts
@@ -5,7 +5,7 @@ import axios from 'axios';
 // Set the base URL for the API
 const axiosInstance = axios.create({
     // todo: change the name from REACT_APP
-    baseURL: import.meta.env.REACT_APP_API_URL ?? 'http://localhost:8000'
+    baseURL: import.meta.env.VITE_API_URL ?? 'http://localhost:8000'
 });
 
 console.log(axiosInstance);
